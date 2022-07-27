3564ab42225decda90e04fcca7851421d1b95fb1
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2022-07-27 16:21:28 +0530

    [ADD] styling , few function
    add little bit of styling and button for erase , clear , random color and fav color.

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const body = document.body;
 const pixelSpace = document.createElement('div');
 const form = document.querySelector('form');
-const clear = document.querySelector('.clear')
+const clear = document.querySelector('.clear');
+const favColor = document.querySelector('.favcolor');
+const randomColor = document.querySelector('.random');
+const content = document.querySelector('.content');
+const erase = document.querySelector('.erase');
 
 
 body.classList.add('body');
 pixelSpace.classList.add('pixelspace');
-body.appendChild(pixelSpace);
+content.appendChild(pixelSpace);
 
 const input = document.querySelector('.input');
 
@@ -15,24 +19,60 @@ form.addEventListener('submit', (e) => {
 
     e.preventDefault();
 
+    // pixelSpace.style.border = "2px solid black";
+
     for( i = 1 ; i<=input.value ; i++ ){
         const pixels = document.createElement('div');
         pixels.classList.add('pixels')
         pixelSpace.appendChild(pixels);
         
-        let r = Math.floor((Math.random() * 250) + 1); 
-        let g = Math.floor((Math.random() * 250) + 1); 
-        let b = Math.floor((Math.random() * 250) + 1); 
+        randomColor.addEventListener('click',() => {
 
-        let bgColor = "rgb(" + r + "," + g + "," + b + ")";
+            let r = Math.floor((Math.random() * 250) + 1); 
+            let g = Math.floor((Math.random() * 250) + 1); 
+            let b = Math.floor((Math.random() * 250) + 1); 
+    
+            let bgColor = "rgb(" + r + "," + g + "," + b + ")";
+    
 
-        pixelSpace.addEventListener('mousemove' , () => {
+            pixelSpace.addEventListener('mousemove' , () => {
 
-            pixels.addEventListener('mouseover', () => {
-                pixels.style.backgroundColor = bgColor;
+                pixels.addEventListener('mouseover', () => {
+                    pixels.style.backgroundColor = bgColor;
+                })
+    
+            })
+        })
+
+        favColor.addEventListener('click' , () => {
+
+            
+            pixelSpace.addEventListener('mousemove' , () => {
+
+                pixels.addEventListener('mouseover', () => {
+                    pixels.style.backgroundColor = favColor.value;
+                })
+    
             })
 
         })
+
+        erase.addEventListener('click' , () => {
+
+            
+            pixelSpace.addEventListener('mousemove' , () => {
+
+                pixels.addEventListener('mouseover', () => {
+                    pixels.style.backgroundColor = 'rgb(255,255,255)';
+                })
+    
+            })
+
+        })
+
+
+       
+       
 
         clear.addEventListener('click', () => {
             pixels.style.backgroundColor = 'rgb(255,255,255)';
